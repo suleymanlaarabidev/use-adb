865b559b94680b086d513c01aa2abd71fe63fa95
suleymanlaarabidev <suleymanlaarabi01@gmail.com>
2023-04-03 23:02:00 +0200

    add all test for classclient of use-adb

--- adb-class-test.js
+++ adb-class-test.js
@@ -12,67 +12,220 @@ process.argv.forEach((val, index) => {
   }
 });
 
-// Test 12: go to home
-function Test12() {
+// Test 25: volume down
+function Test25() {
+  adbClient.volumeDown().then((res) => {
+    if (res.error) {
+      throw new Error("Test 25 failed: unable to volume down");
+    } else {
+      console.log("Test 25 passed: volume down success");
+    }
+  });
+}
+
+// Test 24: volume up
+function Test24() {
+  adbClient.volumeUp().then((res) => {
+    if (res.error) {
+      throw new Error("Test 24 failed: unable to volume up");
+    } else {
+      console.log("Test 24 passed: volume up success");
+      Test25();
+    }
+  });
+}
+
+// Test 23: turn onn/off wifi
+function Test23() {
+  adbClient.turnWifiOnOff("enable").then((res) => {
+    if (res.error) {
+      throw new Error("Test 23 failed: unable to turn on/off wifi");
+    } else {
+      console.log("Test 23 passed: turn on/off wifi success");
+      Test24();
+    }
+  });
+}
+
+// Test 22: wait device is unlocked
+function Test22() {
+  adbClient.waitScreenUnlocked().then((res) => {
+    if (res.error) {
+      throw new Error("Test 22 failed: unable to wait device is unlocked");
+    } else {
+      console.log("Test 22 passed: wait device is unlocked success");
+      Test23();
+    }
+  });
+}
+
+// Test 21: go to password
+function Test21() {
+  adbClient.goToPassword().then((res) => {
+    if (res.error) {
+      throw new Error("Test 21 failed: unable to go to password");
+    } else {
+      console.log("Test 21 passed: go to password success");
+      Test22();
+    }
+  });
+}
+
+// Test 20: check isUnlocked
+function Test20() {
+  adbClient.isUnlocked().then((res) => {
+    if (res.error) {
+      throw new Error("Test 20 failed: unable to check isUnlocked");
+    } else {
+      if (!res) {
+        console.log("Test 20 passed: isUnlocked is false");
+        Test21();
+      } else {
+        throw new Error("Test 20 failed: isUnlocked is true");
+      }
+    }
+  });
+}
+
+// Test 19: turn on screen
+function Test19() {
+  adbClient.turnScreenOnOff().then((res) => {
+    if (res.error) {
+      throw new Error("Test 19 failed: unable to turn on screen");
+    } else {
+      console.log("Test 19 passed: turn on screen success");
+      Test20();
+    }
+  });
+}
+
+// Test 18: turn off screen
+function Test18() {
+  adbClient.turnScreenOnOff().then((res) => {
+    if (res.error) {
+      throw new Error("Test 18 failed: unable to turn off screen");
+    } else {
+      console.log("Test 18 passed: turn off screen success");
+      Test19();
+    }
+  });
+}
+
+// Test 17: if screen is off
+function Test17() {
+  adbClient.isScreenOff().then((res) => {
+    if (res.error) {
+      throw new Error("Test 17 failed: unable to check screen status");
+    } else {
+      console.log("Test 17 passed: screen status is" + res);
+      Test18();
+    }
+  });
+}
+
+// Test 16: take a screenshot
+function Test16() {
+  adbClient.takeScreenshot("testscreen.png").then((res) => {
+    if (res.error) {
+      throw new Error("Test 16 failed: unable to take screenshot");
+    } else {
+      adbClient.listFile("/sdcard/").then((res) => {
+        if (res.error) {
+          throw new Error("Test 16 failed: unable to list files");
+        } else {
+          if (res.includes("testscreen.png")) {
+            console.log("Test 16 passed: take screenshot success");
+            // remove screenshot
+            adbClient.removeFile("/sdcard/testscreen.png").then((res) => {
+              if (res.error) {
+                throw new Error("Test 16 failed: unable to remove screenshot");
+              }
+              Test17();
+            });
+          } else {
+            throw new Error("Test 16 failed: unable to find screenshot");
+          }
+        }
+      });
+    }
+  });
+}
+
+// Test 15: open app tiktok
+function Test15() {
+  adbClient.openApp("com.zhiliaoapp.musically").then((res) => {
+    if (res.error) {
+      throw new Error("Test 15 failed: unable to open app tiktok");
+    } else {
+      console.log("Test 15 passed: open app tiktok success");
+      Test16();
+    }
+  });
+}
+
+// Test 13: go to home
+function Test13() {
   adbClient.goToHome().then((res) => {
     if (res.error) {
       throw new Error("Test 14 failed: unable to go to home");
     } else {
       console.log("Test 14 passed: go to home success");
+      Test15();
     }
   });
 }
 
-// Test 11: go back
-function Test11() {
+// Test 12: go back
+function Test12() {
   adbClient.goBack().then((res) => {
     if (res.error) {
       throw new Error("Test 11 failed: unable to go back");
     } else {
       console.log("Test 11 passed: go back success");
-      Test12();
+      Test13();
     }
   });
 }
 
-// Test 10: open Menu
-function Test10() {
+// Test 11: open Menu
+function Test11() {
   adbClient.openMenu().then((res) => {
     if (res.error) {
       throw new Error("Test 10 failed: unable to open menu");
     } else {
       console.log("Test 10 passed: open menu success");
-      Test11();
+      Test12();
     }
   });
 }
 
-// Test 9: open browser
-function Test9() {
+// Test 10: open browser
+function Test10() {
   adbClient.openBrowser("https://www.google.com").then((res) => {
     if (res.error) {
       throw new Error("Test 9 failed: unable to open browser");
     } else {
       console.log("Test 9 passed: open browser success");
-      Test10();
+
+      Test11();
     }
   });
 }
 
-// Test 8: get device info
-function Test8() {
+// Test 9: get device info
+function Test9() {
   adbClient.getDeviceInfo().then((res) => {
     if (res.error) {
       throw new Error("Test 8 failed: unable to get device info");
     } else {
       console.log("Test 8 passed: get device info success");
-      Test9();
+      Test10();
     }
   });
 }
 
-// Test 7: remove file
-function Test7() {
+// Test 8: remove file
+function Test8() {
   adbClient.removeFile("/sdcard/test.txt").then((res) => {
     if (res.error) {
       throw new Error("Test 7 failed: unable to remove file");
@@ -83,9 +236,31 @@ function Test7() {
         } else {
           if (!res.includes("test.txt")) {
             console.log("Test 7 passed: remove file success");
-            Test8();
+            Test9();
           } else {
             throw new Error("Test 7 failed: file not removed");
+          }
+        }
+      });
+    }
+  });
+}
+
+// Test 7: pull file
+function Test7() {
+  adbClient.pullFile("/sdcard/test.txt", "./assets/").then((res) => {
+    if (res.error) {
+      throw new Error("Test 7 failed: unable to pull file");
+    } else {
+      adbClient.listFile("/sdcard/").then((res) => {
+        if (res.error) {
+          throw new Error("Test 7 failed: unable to list files");
+        } else {
+          if (res.includes("test.txt")) {
+            console.log("Test 7 passed: pull file success");
+            Test8();
+          } else {
+            throw new Error("Test 7 failed: unable to find file");
           }
         }
       });
@@ -132,7 +307,7 @@ function Test5() {
             }
           });
           if (!isInstalled) {
-            console.log("Test 5 passed: apk is removed");
+            console.log("Test 5 passed: apk removed");
             Test6();
           } else {
             throw new Error("Test 5 failed: apk is still installed");
@@ -185,10 +360,16 @@ if (adbClient) {
 }
 
 // Test 2: get device
-adbClient.getDevices().then((devices) => {
-  adbClient.createClient(devices[deviceSelected]);
-  if (adbClient.client) {
-    console.log("Test 2 passed: get the device");
-    Test3();
+adbClient.startServer().then((res) => {
+  if (res.error) {
+    throw new Error("Test 2 failed: unable to start server");
+  } else {
+    adbClient.getDevices().then((devices) => {
+      adbClient.createClient(devices[deviceSelected]);
+      if (adbClient.client) {
+        console.log("Test 2 passed: get the device");
+        Test3();
+      }
+    });
   }
 });
