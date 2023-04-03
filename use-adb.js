36971cde001af76d00683c9fac982174f20cc498
suleymanlaarabidev <suleymanlaarabi01@gmail.com>
2023-04-03 23:02:55 +0200

    add lot of features of classCLient and remove all old deprecated function export

--- use-adb.js
+++ use-adb.js
@@ -20,518 +20,21 @@ if (process.platform == "linux") {
   adbFile = dirname + "/sdk-dir/platform-tools-mac/adb";
 }
 
-// for get devices
-module.exports.getDevices = function (customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    exec(adbFile + " devices", function (error, stdout, stderr) {
-      if (error) {
-        reject(error);
-      } else {
-        let devicesListe = [];
-        var devices = stdout.replace("List of devices attached", "");
-        devices = devices.split("\n");
-        devices.forEach((device) => {
-          if (device.length > 0) {
-            devicesListe.push({
-              name: device.split("\t")[0],
-              status: device.split("\t")[1],
-            });
-          }
-        });
-        resolve(devicesListe);
-      }
-    });
-  });
-};
-
-// for reboot device
-module.exports.reboot = function (mode, devices, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile + " -s " + devices.name + " reboot " + mode,
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: truncate,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: "Device is rebooting in " + mode + " mode",
-            });
-          }
-        }
-      );
-    } else {
-      resolve("Device is not authorized");
-    }
-  });
-};
-
-// for install apk
-module.exports.installApk = function (file, devices, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile + " -s " + devices.name + " install -r " + file,
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: true,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: "app " + file + " is installed",
-            });
-          }
-        }
-      );
-    } else {
-      reject({
-        error: true,
-        data: "Device is not authorized",
-      });
-    }
-  });
-};
-
-// for uninstall apk
-module.exports.uninstallApk = function (package, devices, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile + " -s " + devices.name + " uninstall " + package,
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: true,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: "app " + package + " is uninstalled",
-            });
-          }
-        }
-      );
-    } else {
-      reject({
-        error: true,
-        data: "Device is not authorized",
-      });
-    }
-  });
-};
-
-// for sideload file
-module.exports.sideload = function (file, devices, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile + " -s " + devices.name + " sideload " + file,
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: true,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: "sideload " + file + " sucess",
-            });
-          }
-        }
-      );
-    } else {
-      rejects({
-        error: true,
-        data: "Device is not authorized",
-      });
-    }
-  });
-};
-
-// for push file
-module.exports.push = function (file, devices, to, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile + " -s " + devices.name + " push " + file + " " + to,
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: true,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: "push " + file + " to " + to + " sucess",
-            });
-          }
-        }
-      );
-    } else {
-      reject({
-        error: true,
-        data: "Device is not authorized",
-      });
-    }
-  });
-};
-
-// for pull file
-module.exports.pull = function (file, devices, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile + " -s " + devices.name + " pull " + file,
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: true,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: "pull " + file + " sucess",
-            });
-          }
-        }
-      );
-    } else {
-      reject({
-        error: true,
-        data: "Device is not authorized",
-      });
-    }
-  });
-};
-
-// for rm file
-module.exports.rmFile = function (file, devices, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile + " -s " + devices.name + " shell rm " + file,
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: true,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: "rm file : " + file + " success",
-            });
-          }
-        }
-      );
-    } else {
-      reject({
-        error: true,
-        data: "Device is not authorized",
-      });
-    }
-  });
-};
-
-// for touch file
-module.exports.touchFile = function (file, devices, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile + " -s " + devices.name + " shell touch " + file,
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: true,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: "touch file : " + file + " success",
-            });
-          }
-        }
-      );
-    } else {
-      reject({
-        error: true,
-        data: "Device is not authorized",
-      });
-    }
-  });
-};
-
-// for mkdir file
-module.exports.ls = function (devices, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile + " -s " + devices.name + " shell ls /sdcard",
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: true,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: stdout,
-            });
-          }
-        }
-      );
-    } else {
-      reject({
-        error: true,
-        data: "Device is not authorized",
-      });
-    }
-  });
-};
-
-// for get packages
-module.exports.getPackages = function (devices, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile + " -s " + devices.name + " shell pm list packages",
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: true,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: stdout,
-            });
-          }
-        }
-      );
-    } else {
-      reject({
-        error: true,
-        data: "Device is not authorized",
-      });
-    }
-  });
-};
-
-// home button
-module.exports.home = function (devices, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile + " -s " + devices.name + " shell input keyevent 3",
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: true,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: "home button success",
-            });
-          }
-        }
-      );
-    } else {
-      reject({
-        error: true,
-        data: "Device is not authorized",
-      });
-    }
-  });
-};
-
-// back button
-module.exports.back = function (devices, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile + " -s " + devices.name + " shell input keyevent 4",
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: true,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: "back button success",
-            });
-          }
-        }
-      );
-    } else {
-      reject({
-        error: true,
-        data: "Device is not authorized",
-      });
-    }
-  });
-};
-
-// turn on/off screen
-module.exports.screen = function (devices, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile + " -s " + devices.name + " shell input keyevent 26",
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: true,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: "screen button success",
-            });
-          }
-        }
-      );
-    } else {
-      reject({
-        error: true,
-        data: "Device is not authorized",
-      });
-    }
-  });
-};
-
-// start browser
-module.exports.browser = function (devices, customPath) {
-  // check if a custom path for adb process has been filled
-  if (customPath) {
-    // set custom path for adb
-    adbFile = customPath + "adb";
-  }
-  return new Promise(function (resolve, reject) {
-    if (devices.status == "device" || devices.status == "recovery") {
-      exec(
-        adbFile +
-          " -s " +
-          devices.name +
-          " shell am start -a android.intent.action.VIEW -d https://google.com",
-        function (error, stdout, stderr) {
-          if (error) {
-            reject({
-              error: true,
-              data: error,
-            });
-          } else {
-            resolve({
-              error: false,
-              data: "browser button success",
-            });
-          }
-        }
-      );
-    } else {
-      reject({
-        error: true,
-        data: "Device is not authorized",
-      });
-    }
-  });
-};
-
 module.exports.adbClient = class {
   // the client object variable
 
   constructor(device) {
     this.client = null;
+    this.isStarded = false;
     // start the server if not started
-    this.startServer();
+    this.startServer().then((res) => {
+      if (res.error) {
+        throw new Error("Unable to start server");
+      } else {
+        this.isStarded = true;
+      }
+    });
+
     // check if a device is filled
     if (device) {
       // create client
@@ -544,8 +47,12 @@ module.exports.adbClient = class {
     return new Promise((resolve, reject) => {
       exec(adbFile + " start-server", function (error, stdout, stderr) {
         if (error) {
+          // set isStarted to false
+
           reject(error);
         } else {
+          // set isStarted to true
+
           resolve(stdout);
         }
       });
@@ -941,6 +448,178 @@ module.exports.adbClient = class {
             " shell monkey -p " +
             packageName +
             " -c android.intent.category.LAUNCHER 1",
+          function (error, stdout, stderr) {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(stdout);
+            }
+          }
+        );
+      } else {
+        reject("Client is not defined");
+      }
+    });
+  }
+
+  // function for take a screenshot
+  takeScreenshot(name) {
+    return new Promise((resolve, reject) => {
+      if (this.client) {
+        exec(
+          adbFile +
+            " -s " +
+            this.client.name +
+            " shell screencap -p /sdcard/" +
+            name,
+          function (error, stdout, stderr) {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(stdout);
+            }
+          }
+        );
+      } else {
+        reject("Client is not defined");
+      }
+    });
+  }
+
+  // get if screen is off
+  isScreenOff() {
+    return new Promise((resolve, reject) => {
+      if (this.client) {
+        exec(
+          adbFile + " -s " + this.client.name + " shell dumpsys power",
+          function (error, stdout, stderr) {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(stdout.includes("mScreenOn=false"));
+            }
+          }
+        );
+      } else {
+        reject("Client is not defined");
+      }
+    });
+  }
+
+  // check is unlocked
+  isUnlocked() {
+    return new Promise((resolve, reject) => {
+      if (this.client) {
+        exec(
+          adbFile + " -s " + this.client.name + " shell dumpsys window policy",
+          function (error, stdout, stderr) {
+            if (error) {
+              reject(error);
+            } else {
+              // get status mIsShowing
+              let status = stdout.split("mIsShowing=")[1];
+              status = status.split(" ")[0];
+              if (status.trim() == "true") {
+                resolve(false);
+              } else {
+                resolve(true);
+              }
+            }
+          }
+        );
+      } else {
+        reject("Client is not defined");
+      }
+    });
+  }
+
+  // function go to password
+  goToPassword() {
+    return new Promise((resolve, reject) => {
+      if (this.client) {
+        exec(
+          adbFile + " -s " + this.client.name + " shell input keyevent 82",
+          function (error, stdout, stderr) {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(stdout);
+            }
+          }
+        );
+      } else {
+        reject("Client is not defined");
+      }
+    });
+  }
+
+  // function wait screen is unlocked and return
+  waitScreenUnlocked() {
+    return new Promise((resolve, reject) => {
+      if (this.client) {
+        this.isUnlocked().then((unlocked) => {
+          if (unlocked) {
+            resolve(true);
+          } else {
+            setTimeout(() => {
+              this.waitScreenUnlocked().then((res) => {
+                resolve(res);
+              });
+            }, 1000);
+          }
+        });
+      } else {
+        reject("Client is not defined");
+      }
+    });
+  }
+
+  // function for turn on-off wifi
+  turnWifiOnOff(mode) {
+    return new Promise((resolve, reject) => {
+      if (this.client) {
+        exec(
+          adbFile + " -s " + this.client.name + " shell svc wifi " + mode,
+          function (error, stdout, stderr) {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(stdout);
+            }
+          }
+        );
+      } else {
+        reject("Client is not defined");
+      }
+    });
+  }
+
+  // function for up volume
+  volumeUp() {
+    return new Promise((resolve, reject) => {
+      if (this.client) {
+        exec(
+          adbFile + " -s " + this.client.name + " shell input keyevent 24",
+          function (error, stdout, stderr) {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(stdout);
+            }
+          }
+        );
+      } else {
+        reject("Client is not defined");
+      }
+    });
+  }
+
+  // function for down volume
+  volumeDown() {
+    return new Promise((resolve, reject) => {
+      if (this.client) {
+        exec(
+          adbFile + " -s " + this.client.name + " shell input keyevent 25",
           function (error, stdout, stderr) {
             if (error) {
               reject(error);
